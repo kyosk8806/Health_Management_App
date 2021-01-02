bab6f5d98c1b62cd0e0e3bc1a4782a338404a69d
kyosk8806 <kkusaka6_2@yahoo.co.jp>
2021-01-02 18:52:07 +0900

    Changed buttons to icons

--- resources/views/Records/index.blade.php
+++ resources/views/Records/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+
 <div class="container">
     <!-- Header -->
     <div class="row header">
@@ -22,9 +24,9 @@
         <div class="row justify-content-center">
             <div class="col-md-12" style="margin-bottom:20px">
                 <div class="header text-center">
-                    <a class="btn btn-success" href="../{{ $prev_month }}/{{ $prev_year }}"><<</a>
+                    <a class="btn btn-success" href="../{{ $prev_month }}/{{ $prev_year }}"><i class="fas fa-angle-double-left"></i></a>
                     <span>{{ $month }} / {{ $year }}</span>
-                    <a class="btn btn-success" href="../{{ $next_month }}/{{ $next_year }}">>></a>
+                    <a class="btn btn-success" href="../{{ $next_month }}/{{ $next_year }}"><i class="fas fa-angle-double-right"></i></a>
                 </div>
             </div>
         </div>
@@ -34,33 +36,35 @@
         <table class="table" id="datatable" >
             <thead>
                 <tr class="table_scope">
-                    <th class="date" scope="col" width="20%">Date</th>
-                    <th class="weight" scope="col" width="15%">Weight</th>
-                    <th class="steps" scope="col" width="15%">Steps</th>
-                    <th class="exercise" scope="col" width="15%">Exercise</th>
-                    <th class="notes" scope="col" width="25%">Notes</th>
-                    <th class="edit" scope="col" width="5%">Edit</th>
-                    <th class="del" scope="col" width="5%"></th>
+                    <th scope="col" width="20%">Date</th>
+                    <th scope="col" width="15%">Weight</th>
+                    <th scope="col" width="15%">Steps</th>
+                    <th scope="col" width="15%">Exercise</th>
+                    <th scope="col" width="25%">Notes</th>
+                    <th scope="col" width="5%">Action</th>
+                    <th scope="col" width="5%"></th>
                 </tr>
             </thead>
 
             <tbody>            
             @foreach ($records as $record)
                 <tr class="table_data">
-                    <th class="date" scope="row">{{ $record->date }}</th>
-                    <td class="weight">{{ $record->weight }}kg</td>
-                    <td class="step">{{ $record->step }}steps</td>
-                    <td class="exercise">{{ $record->exercise }}</td>
-                    <td class="note">{{ $record->note }}</td>
-                    <td class="edit">
-                        <a href="{{ route('records.edit', $record->id) }}" class="btn btn-primary edit">Edit</a>
+                    <th>{{ $record->date }}</th>
+                    <td>{{ $record->weight }}kg</td>
+                    <td>{{ $record->step }}steps</td>
+                    <td>{{ $record->exercise }}</td>
+                    <td>{{ $record->note }}</td>
+                    <td>
+                        <a href="{{ route('records.edit', $record->id) }}" class="btn btn-outline-primary edit"><i class="fas fa-edit"></i></a>
                     </td>
-                    <td class="del">
-                        <form action="{{ route('records.destroy', $record->id) }}" method="POST"　onsubmit="return delete_confirm()">
+                    <td>
+                        <!-- <form action="{{ route('records.destroy', $record->id) }}" method="POST"　onsubmit="return delete_confirm()">
                             @csrf
                             @method('DELETE')
                             <input type="submit" value="Del" class="btn btn-danger">
-                        </form>
+                        </form> -->
+                        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash-alt"></i>
+                        </button>
                     </td>
                 </tr>
                 @endforeach
@@ -117,6 +121,36 @@
 </div>
 <!-- End Create Modal -->
 
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Delete Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('records.destroy', $record->id) }}" method="POST" id="deleteForm">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body">
+
+                    <input type="hidden" name="_method" value="DELETE"/>
+                    <p>Are you sure? You want to delete data.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+{{--End Delete Modal--}}
+
 <!-- Edit Modal -->
 <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -165,39 +199,12 @@
 </div>
 <!-- End Edit Modal -->
 
-<script type="text/javascript">
 
-    $(document).ready(function () {
-
-        var table = $('#datatable').DataTable();
-
-        table.on('click', '.edit', function() {
-            $tr = $(this).closest('tr');
-            if ($($tr).hasClass('child')) {
-                $tr = $tr.prev('.parent');
-            }
-
-            var data = table.row($tr).data();
-            console.log(data);
-
-            // $('#datepicker').val(data[1]);
-            $('#weight').val(data[2]);
-            // $('#step').val(data[3]);
-            // $('#sel02').val(data[4]);
-            // $('#note').val(data[5]);
-
-            $('#editForm').attr('action', '/records/edit');
-            $('#editModal').modal('show');
-
-
-        });
-    });
-
-
-// function delete_confirm() {
-//     var select = confirm("Are you sure you want to delete this item?");
-//     return select;
-// }
-</script>
+<!-- <script>
+function delete_confirm() {
+    var select = confirm("Are you sure you want to delete this item?");
+    return select;
+}
+</script> -->
 
 @endsection
